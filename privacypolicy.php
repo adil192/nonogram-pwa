277f4dc5891be12c69f72918ce09b902e68ad422
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-05-14 04:33:54 +0100

    nonogram: add privacy policy

---
+++ privacypolicy.php
@@ -0,0 +1,89 @@
+<?php
+include_once "../global_tools.php";
+// https://www.docracy.com/6016/mobile-privacy-policy#
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<?php createHead(
+		"Privacy Policy - Nonogram",
+		"Play unlimited randomly generated nonograms, graphical crosswords where you colour in a grid according to the numbers on the rows and columns.",
+		null,
+		null,
+		"2022-05-06",
+		"InteractiveResource",
+		true
+	); ?>
+
+	<link rel="stylesheet" href="/assets/ext/bootstrap.5.1.3.min.css" />
+</head>
+<body>
+
+
+<header>
+	<h1>Privacy Policy - Nonogram</h1>
+</header>
+
+<main>
+    This privacy policy governs your use of the software application Nonogram for mobile and desktop devices that was
+    created by Adil Hanney. The Application is a game based on nonograms (picture logic puzzles).<br>
+
+    <br><h2>What information does the Application obtain and how is it used?</h2>
+
+    <br><h3>User Provided Information</h3>
+
+    The Application does not require or ask for you to register with us, nor asks for any personal information.<br>
+
+    <br><h3>Automatically Collected Information</h3>
+
+    The Application may collect certain information automatically, including, but not limited to, the
+    type of device you use, the IP address of your device, your operating system, the type of Internet browser you
+    use, and logs of which pages of the Application you access.
+    This information is sent by most modern Internet browsers automatically, and will not be used to identify you.<br>
+
+    <br><h3>Does the Application collect precise real time location information of the device?</h3>
+
+    This Application does not collect precise information about the location of your mobile device.<br>
+
+    <br><h3>Do third parties see and/or have access to information obtained by the Application?</h3>
+
+    Third parties do not have access to any of your information, with the exception of the Application's hosting
+    service which necessarily has access to any User Provided and Automatically Collected Information.<br>
+
+    <br><h3>What are my opt-out rights?</h3>
+
+    You can stop all collection of information by the Application easily by uninstalling the Application, or
+    by ceasing to use the Application.
+    You may use the standard uninstall processes as may be available as part of your mobile device or
+    via the mobile application marketplace or network.<br>
+
+    <br><h3>Data Retention Policy, Managing Your Information</h3>
+
+    We will retain Automatically Collected information for up to 24 months and thereafter may store it in aggregate.<br>
+
+    <br><h3>Changes</h3>
+
+    This Privacy Policy may be updated from time to time for any reason.
+    We will notify you of any changes to our Privacy Policy by posting the new Privacy Policy here at
+    <a href="https://adil.hanney.org/nonogram/privacypolicy.php">https://adil.hanney.org/nonogram/privacypolicy.php</a>.
+    You are advised to consult this Privacy Policy regularly for any changes,
+    as continued use is deemed approval of all changes.<br>
+
+    <br><h3>Your Consent</h3>
+
+    By using the Application, you are consenting to our processing of your information
+    as set forth in this Privacy Policy now and as amended by us.
+    "Processing,” means using cookies on a computer/hand held device or using or touching information in any way,
+    including, but not limited to, collecting, storing, deleting, using, combining and disclosing information,
+    all of which activities will take place in the United Kingdom. If you reside outside the United Kingdom
+    your information will be transferred, processed and stored there under United Kingdom privacy standards.<br>
+
+    <br><h3>Contact us</h3>
+
+    If you have any questions regarding privacy while using the Application,
+    or have questions about our practices, please contact us via email at
+    <a href="mailto:adilhanney@gmail.com">adilhanney@gmail.com</a>.<br>
+</main>
+
+</body>
+</html>
