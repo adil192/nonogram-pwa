6aeecc3ab2341e4f5a4ac33928425e897091d328
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-06-07 08:57:15 +0100

    nonogram: bump version

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once "../global_tools.php";
-$lastUpdate = "22-06-06-0700"; // when changing this, you should also update sw.js
+$lastUpdate = "22-06-07-0800"; // when changing this, you should also update sw.js
 ?>
 <!doctype html>
 <html lang="en">
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name has a timestamp because the browser re-caches the assets when the service worker file is modified
-const staticCacheName = "nonogram-cache-" + "22-06-06-0700";
+const staticCacheName = "nonogram-cache-" + "22-06-07-0800";
 const assets = [
 	'/nonogram/',
 	'/nonogram/nonogram.css',
