f2779a17f324dee235be4d8d41ae784d24e1f7dc
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-06-26 17:45:12 +0100

    nonogram: bump version

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name has a timestamp because the browser re-caches the assets when the service worker file is modified
-const staticCacheName = "nonogram-cache-" + "22-06-25-2300";
+const staticCacheName = "nonogram-cache-" + "22-06-26-0000";
 const assets = [
 	'/nonogram/',
 	'/nonogram/nonogram.css',
