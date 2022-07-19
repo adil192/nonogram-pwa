40ff6262844faae232776148a96459ae1344e73d
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-07-19 15:15:24 +0100

    quintle: update wordlists

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Cache name has a timestamp because the browser re-caches the assets when the service worker file is modified
-const staticCacheName = "nonogram-cache-" + "22-07-15-0300";
+const staticCacheName = "nonogram-cache-" + "22-07-15-0400";
 const assets = [
 	'/nonogram/',
 	'/nonogram/nonogram.css',
