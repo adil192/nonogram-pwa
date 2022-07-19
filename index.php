6b448090630520d4a67ea6362440650d953f9ca3
Adil Hanney <adilh+github@orchid.anonaddy.com>
2022-07-20 00:41:23 +0100

    nonogram: bump version

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once "../global_tools.php";
-$lastUpdate = "22-07-15-0300"; // when changing this, you should also update sw.js
+$lastUpdate = "22-07-19-2359"; // when changing this, you should also update sw.js
 ?>
 <!doctype html>
 <html lang="en">
